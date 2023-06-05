ec5d0028874a31796341fada716b9a1dda40f3ca
Luftmensch <thomas.23333@gmail.com>
2023-06-04 20:54:08 -0500

    [add] displaying page

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def copy():
+    return render_template('copy.html')
+
+@app.route('/<text>')
+def paste(text):
+    return render_template('paste.html', **locals())
