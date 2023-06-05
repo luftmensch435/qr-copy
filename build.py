f049461324a44540f811c4bf74b1a770013fbc37
Luftmensch <thomas.23333@gmail.com>
2023-06-05 00:24:11 -0500

    [fix] move build to docs

--- build.py
+++ build.py
@@ -1,7 +1,22 @@
+import shutil
+import os
 from flask_frozen import Freezer
 from app import app
+
+def move():
+    src = "build"
+    dst = "docs"
+    try:
+        # if path already exists, remove it before copying with copytree()
+        if os.path.exists(dst):
+            shutil.rmtree(dst)
+            shutil.copytree(src, dst)
+    except Exception as e:
+        print('Directory not copied. Error: %s' % e)
+
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
     freezer.freeze()
+    move()
